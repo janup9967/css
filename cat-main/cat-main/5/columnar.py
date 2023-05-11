cf6c4c0ebd28e9a6b0a2baf418aa7796e7bf9d45
Anup Jaiswal <94697354+janup9967@users.noreply.github.com>
2023-05-11 22:44:57 +0530

    Update columnar.py

--- cat-main/cat-main/5/columnar.py
+++ cat-main/cat-main/5/columnar.py
@@ -1,45 +1,112 @@
-def columnar_encryption(text, key):
-  ct = ''
-  arr = [[None for i in range(len(key))] for j in range(len(key))]
+import math
 
-  k = 0
-  for i in range(len(key)):
-    for j in range(len(key)):
-      if k < len(text):
-        arr[i][j] = text[k]
-        k +=1
-      else:
-        arr[i][j] = 'X'
- 
-  for i in key:
-    for j in range(len(key)):
-      
-      ct += arr[j][int(i)-1]
-  return ct, arr
-  
-def columnar_decryption(ct, key):
-  text = ''
-  arr = [[None for i in range(len(key))] for j in range(len(key))]
-  k = 0
-  for i in key:
-    for j in range(len(key)):
-      if k < len(ct):
-        arr[j][int(i)-1] = ct[k]
-        k +=1
-      else:
-        arr[i][j] = ''
-  print(arr)
-  for i in range(len(key)):
-    for j in range(len(key)):
-      if arr[i][j] != 'X':
-        text += arr[i][j]
-  
-  return text, arr
+key = "zebra"
 
+# Encryption
+def encryptMessage(msg):
+	cipher = ""
 
-text = input("Enter the message: ")
-n = input("Enter the key: ")
-encrypt_message=columnar_encryption(text, n)
-print("Encode Text: ",encrypt_message[0])
+	# track key indices
+	k_indx = 0
 
-print("Decode Text: ",columnar_decryption(encrypt_message[0],n)[0])
+	msg_len = float(len(msg))
+	msg_lst = list(msg)
+	key_lst = sorted(list(key))
+
+	# calculate column of the matrix
+	col = len(key)
+	# print(col)
+	
+	# calculate maximum row of the matrix
+	row = int(math.ceil(msg_len / col))
+	# print(row)
+
+	# add the padding character '_' in empty
+	# the empty cell of the matix
+	fill_null = int((row * col) - msg_len)
+	msg_lst.extend('_' * fill_null)
+	# print(msg_lst)
+
+	# create Matrix and insert message and
+	# padding characters row-wise
+	matrix = [msg_lst[i: i + col]
+			for i in range(0, len(msg_lst), col)]
+
+	print(matrix)
+	# read matrix column-wise using key
+	for _ in range(col):
+		curr_idx = key.index(key_lst[k_indx])
+		cipher += ''.join([row[curr_idx]
+						for row in matrix])
+		k_indx += 1
+
+	return cipher
+
+# Decryption
+def decryptMessage(cipher):
+	msg = ""
+
+	# track key indices
+	k_indx = 0
+
+	# track msg indices
+	msg_indx = 0
+	msg_len = float(len(cipher))
+	msg_lst = list(cipher)
+
+	# calculate column of the matrix
+	col = len(key)
+	
+	# calculate maximum row of the matrix
+	row = int(math.ceil(msg_len / col))
+
+	# convert key into list and sort
+	# alphabetically so we can access
+	# each character by its alphabetical position.
+	key_lst = sorted(list(key))
+
+	# create an empty matrix to
+	# store deciphered message
+	dec_cipher = []
+	for _ in range(row):
+		dec_cipher += [[None] * col]
+
+	# Arrange the matrix column wise according
+	# to permutation order by adding into new matrix
+	for _ in range(col):
+		curr_idx = key.index(key_lst[k_indx])
+
+		for j in range(row):
+			dec_cipher[j][curr_idx] = msg_lst[msg_indx]
+			msg_indx += 1
+		k_indx += 1
+
+	# convert decrypted msg matrix into a string
+	try:
+		msg = ''.join(sum(dec_cipher, []))
+	except TypeError:
+		raise TypeError("This program cannot",
+						"handle repeating words.")
+
+	null_count = msg.count('_')
+
+	if null_count > 0:
+		return msg[: -null_count]
+
+	return msg
+
+# Driver Code
+msg = input("Enter message: ")
+
+cipher = encryptMessage(msg)
+print("Encrypted Message: {}".
+			format(cipher))
+
+print("Decryped Message: {}".
+	format(decryptMessage(cipher)))
+
+# output
+# Enter message: cryptography
+# [['c', 'r', 'y', 'p', 't'], ['o', 'g', 'r', 'a', 'p'], ['h', 'y', '_', '_', '_']]
+# Encrypted Message: tp_yr_rgypa_coh
+# Decryped Message: cryptography
